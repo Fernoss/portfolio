333375cdbc1e51265e2bb422d285273f5b60800a
Joonas Fernando Koskinen <joonas.f.koskinen@gmail.com>
2023-05-01 18:06:33 +0300

    Update......0.9999991234

--- script.js
+++ script.js
@@ -12,28 +12,28 @@ const projectPtext = document.querySelector('.projectbtn p');
 // Object for the window
 const projectContent = [
   {
+    h1: 'Today-I-Learned',
+    p: '⚛️ A web development project that combines basics of JS, HTML, CSS. First we use HTML to build the skeleton. Then with CSS we style it and make the layout. We use Supabase to create online db and API for our application data. Then we build the web app with React. Finally, we deploy the final project to a free service called Netlify.',
+    url: 'https://fernoss-todayilearned.netlify.app/',
+  },
+  {
     h1: 'Modal Window',
     p: '👨🏻‍💻 Learning the basics of javaScript using DOM.',
     url: 'https://fernoss.github.io/js-course/modal/',
   },
   {
     h1: 'Guess My Number!',
-    p: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    p: '❓ A number guessing game built with javaScript to take the DOM skills a bit further. For example: Keeping a highscore, restarting the game and manipulating elements to change the layout.',
     url: 'https://fernoss.github.io/js-course/guess-my-number/',
   },
   {
     h1: 'Pig Dice Game',
-    p: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    p: '🎲 A game of dice, where there are two players and the first one to reach 100 points wins! Getting a number 1 resets your current points. Using the skills from previous project ("Guess My Number") a step forward.',
     url: 'https://fernoss.github.io/js-course/pig-game/',
   },
   {
-    h1: 'Test Window 4',
-    p: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    url: '',
-  },
-  {
-    h1: 'Test Window 5',
-    p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    h1: '',
+    p: '',
     url: '',
   },
 ];
